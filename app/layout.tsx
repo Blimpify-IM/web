bb51d2b6e4b1d0934f0dc087ebe9c31adeb475b4
William Hassinen <wassinen04@gmail.com>
2026-02-19 00:36:41 +0000

    meta pixel

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,11 +2,12 @@
 import './globals.css';
 import type { Metadata } from 'next';
 import { designSnippet } from '@blimpify-im/ui/design';
-import { ConsentProvider, CookieConsent, MarketingPixels } from '@blimpify-im/ui/consent';
+import { ConsentProvider, CookieConsent } from '@blimpify-im/ui/consent';
 import { Footer } from '@/components/layout/Footer';
 import { NavbarBar } from '@/components/layout/NavbarBar';
 import { HashScrollHandler } from '@/components/HashScrollHandler';
 import { generateMetadata } from '@/lib/seo';
+import { MarketingPixels } from '@/components/analytics/MarketingPixels';
 
 export const metadata: Metadata = {
   ...generateMetadata({
@@ -76,40 +77,6 @@ export default async function RootLayout({
           src="https://www.googletagmanager.com/gtag/js?id=G-LY6YMV89SC"
         />
 
-        {/* Meta Pixel Code */}
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              !function(f,b,e,v,n,t,s)
-              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-              n.queue=[];t=b.createElement(e);t.async=!0;
-              t.src=v;s=b.getElementsByTagName(e)[0];
-              s.parentNode.insertBefore(t,s)}(window, document,'script',
-              'https://connect.facebook.net/en_US/fbevents.js');
-              fbq('init', '25952653144390900');
-              fbq('track', 'PageView');
-            `,
-          }}
-        />
-
-        {/* TikTok Pixel Code */}
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              !function (w, d, t) {
-                w.TiktokAnalyticsObject=t;var ttq=w[t]=w[t]||[];ttq.methods=["page","track","identify","instances","debug","on","off","once","ready","alias","group","enableCookie","disableCookie","holdConsent","revokeConsent","grantConsent"],ttq.setAndDefer=function(t,e){t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}};
-                for(var i=0;i<ttq.methods.length;i++)ttq.setAndDefer(ttq,ttq.methods[i]);ttq.instance=function(t){for(
-                var e=ttq._i[t]||[],n=0;n<ttq.methods.length;n++)ttq.setAndDefer(e,ttq.methods[n]);return e},ttq.load=function(e,n){var r="https://analytics.tiktok.com/i18n/pixel/events.js",o=n&&n.partner;ttq._i=ttq._i||{},ttq._i[e]=[],ttq._i[e]._u=r,ttq._t=ttq._t||{},ttq._t[e]=+new Date,ttq._o=ttq._o||{},ttq._o[e]=n||{};n=document.createElement("script")
-                ;n.type="text/javascript",n.async=!0,n.src=r+"?sdkid="+e+"&lib="+t;e=document.getElementsByTagName("script")[0];e.parentNode.insertBefore(n,e)};
-                ttq.load('D695FVBC77U5LKV8ML90');
-                ttq.page();
-              }(window, document, 'ttq');
-            `,
-          }}
-        />
-
         {/* Client-side theme resolution script (must run before body renders) */}
         {design.themeMode === 'system' && (
           <script
@@ -135,18 +102,9 @@ export default async function RootLayout({
         )}
       </head>
       <body suppressHydrationWarning>
-        {/* Meta Pixel noscript fallback */}
-        <noscript>
-          <img
-            height="1"
-            width="1"
-            style={{ display: 'none' }}
-            src="https://www.facebook.com/tr?id=25952653144390900&ev=PageView&noscript=1"
-          />
-        </noscript>
+        <MarketingPixels />
         <ConsentProvider>
           {/* GA4 laddas redan i head (kräver inte consent). Övriga pixels (Meta, TikTok, Snap) kan läggas här om consent behövs. */}
-          <MarketingPixels pixels={[]} />
           <HashScrollHandler />
         <NavbarBar />
         <main>{children}</main>
